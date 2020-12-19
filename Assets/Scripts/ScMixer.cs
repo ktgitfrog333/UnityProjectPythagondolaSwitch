f2abf943a3f6c4d93430fb019ac6659b3f1c592a
ktgitfrog333 <paraysis+git@gmail.com>
2020-12-19 17:15:27 +0900

    ・対象オブジェクトの子要素としてエフェクトを作成 Assets\Scenes\ScStage.unity ・UIエフェクト設定とエフェクト発生処理の追加 Assets\Scripts\ScMixer.cs ・エフェクト演出制御処理を作成 Assets\Scripts\Commons\CsNormalLogicDesignOfHitObject.cs

--- Assets/Scripts/ScMixer.cs
+++ Assets/Scripts/ScMixer.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ScMixer : MonoBehaviour
 {
     /// <summary>共通ロジック</summary>
     CsNormalLogicDesignOfCommon logicCommon;
+    /// <summary>エフェクト制御をUIで設定する</summary>
+    [SerializeField] private TriggerEvent onCollisionEnter = new TriggerEvent();
+    /// <summary>エフェクト制御ロジック</summary>
+    CsNormalLogicDesignOfHitObject logicHitObject;
 
     // Start is called before the first frame update
     void Start()
     {
         this.logicCommon = new CsNormalLogicDesignOfCommon();
+        this.logicHitObject = new CsNormalLogicDesignOfHitObject();
     }
 
     // Update is called once per frame
@@ -31,7 +37,15 @@ public class ScMixer : MonoBehaviour
                 FixedJoint f = gameObject.GetComponent<FixedJoint>();
                 f.connectedBody = r;
                 logicCommon.GetComponentScriptInGameObject<ScOperationPlayer>(CsNormalLevelDesignOfBall.GAMEOBJECTS_SP_BALL).OnCollisionEnterParent(this.gameObject);
+                logicHitObject.DinamicParticleOfStartSize(this.gameObject, logicHitObject.MeasureHitSpeedAndFixedLimit(col));
+                onCollisionEnter.Invoke(col);
             }
         }
+    }
+
+    // UnityEventを継承したクラスに[Serializable]属性を付与することで、Inspectorウインドウ上に表示できるようになる。
+    [System.Serializable]
+    public class TriggerEvent : UnityEvent<Collision>
+    {
     }
 }
